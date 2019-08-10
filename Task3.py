9d0e24d35fca1f0d531b0c3cbb6b3a4f37cb58c8
Aniri2013 <aniri-13.rodina@yandex.ru>
2019-08-10 14:25:45 +0300

    commit

--- Task3.py
+++ Task3.py
@@ -3,8 +3,11 @@ B = int(input("Введите B - "))
 C = int(input("Введите C - "))
 if A + B > C and A + C > B and B + C > A:
     print("Треугольник существует")
+    P = (A + B + C) / 2
+    S = (P * (P - A) * (P - B) * (P - C)) ** 0.5
+    H1 = 2 * S / A
+    H2 = 2 * S / B
+    H3 = 2 * S / C
+    print(max(H1, H2, H3))
 else:
     print("Треугольник не существует")
-
-P = (A + B + C) / 2
-print(P)
